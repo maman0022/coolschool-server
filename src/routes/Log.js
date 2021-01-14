381572d7f7211ba64af31f4a1a32cbc7ccf057b4
Muhammad Aman <maman0022@outlook.com>
2021-01-14 12:55:34 -0600

    fixed ip logging

--- src/routes/Log.js
+++ src/routes/Log.js
@@ -6,7 +6,7 @@ const Log = express.Router()
 Log
   .route('/')
   .post((req, res, next) => {
-    const data = req.body
+    const { data } = req.body
     if (!data) {
       return res.status(400).json({ message: 'Data is required' })
     }
@@ -24,6 +24,8 @@ Log
     }
     const body = data.replace(/,/g, '\n').replace(/^\{/, '').replace(/\}$/, '')
     axios.post('https://content.dropboxapi.com/2/files/upload', body, { headers })
+      .then(() => res.status(200).end())
+      .catch(next)
   })
-
+0
 module.exports = Log
